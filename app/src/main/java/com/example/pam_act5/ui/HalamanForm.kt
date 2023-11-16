ef139b62c6d18a52e0961e7bb25302458caf565f
DESTA ATIKANANDA <d.atikananda.ft21@mail.umy.ac.id>
2023-11-16 11:42:31 +0700

    add komponen column form data pelanggan

--- app/src/main/java/com/example/pam_act5/ui/HalamanForm.kt
+++ app/src/main/java/com/example/pam_act5/ui/HalamanForm.kt
@@ -1,11 +1,19 @@
 package com.example.pam_act5.ui
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Preview
 @Composable
@@ -13,4 +21,16 @@ fun HalamanForm() {
     var textNama by remember { mutableStateOf("") }
     var textAlamat by remember { mutableStateOf("") }
     var textTlp by remember { mutableStateOf("") }
+
+    var ListDataTxt: MutableList<String> = mutableListOf(textNama, textAlamat, textTlp)
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxSize()
+    ) {
+        Text(text = "Data Pelanggan")
+    }
 }
