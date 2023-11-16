54091a951f6bb7a50b7a1daf8448f8e425fdbfb7
DESTA ATIKANANDA <d.atikananda.ft21@mail.umy.ac.id>
2023-11-16 11:08:50 +0700

    add method update pada state UI

--- app/src/main/java/com/example/pam_act5/data/OrderUIState.kt
+++ app/src/main/java/com/example/pam_act5/data/OrderUIState.kt
@@ -1,7 +1,9 @@
 package com.example.pam_act5.data
-
 data class OrderUIState(
     val jumlah: Int = 0,
     val rasa: String = "",
     val harga: String = "",
+    val nama: String = "",
+    val alamat: String = "",
+    val noTelp: String = ""
 )
