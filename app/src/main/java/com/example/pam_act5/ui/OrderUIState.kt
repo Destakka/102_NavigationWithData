8711a779db76570476757c573774be25c139e5d5
DESTA ATIKANANDA <d.atikananda.ft21@mail.umy.ac.id>
2023-11-16 10:58:13 +0700

    add data class

---
+++ app/src/main/java/com/example/pam_act5/ui/OrderUIState.kt
@@ -0,0 +1,10 @@
+package com.example.pam_act5.ui
+
+data class OrderUIState(
+    val jumlah: Int = 0,
+    val rasa: String = "",
+    val harga: String = "",
+    val nama: String = "",
+    val alamat: String = "",
+    val noTelp: String = ""
+)
