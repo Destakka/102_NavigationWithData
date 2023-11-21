b4ea724d5d4fdafee3d1bd1fb6488bb35d4f4697
DESTA ATIKANANDA <d.atikananda.ft21@mail.umy.ac.id>
2023-11-22 00:22:27 +0700

    add komponen row untuk button cancle n submit

--- app/src/main/java/com/example/pam_act5/ui/HalamanForm.kt
+++ app/src/main/java/com/example/pam_act5/ui/HalamanForm.kt
@@ -2,11 +2,13 @@ package com.example.pam_act5.ui
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
@@ -18,16 +20,20 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.pam_act5.R
 
 @OptIn(ExperimentalMaterial3Api::class)
-@Preview
 @Composable
-fun HalamanForm() {
+fun HalamanForm(
+    onSubmitButtonClick: (MutableList<String>) -> Unit,
+    onCancelButtonClick: (MutableList<String>) -> Unit
+) {
     var textNama by remember { mutableStateOf("") }
     var textAlamat by remember { mutableStateOf("") }
     var textTlp by remember { mutableStateOf("") }
@@ -79,5 +85,21 @@ fun HalamanForm() {
             label = { Text(text = "Nomor Telepon") }
         )
         Spacer(modifier = Modifier.padding(16.dp))
+
+        Row {
+            Button(onClick = { /*TODO*/ }) {
+                Button(onClick = { onCancelButtonClick(ListDataTxt) }) {
+                    Text(text = stringResource(id = R.string.cancle))
+                }
+
+                Spacer(modifier = Modifier.padding(50.dp))
+
+                Button(onClick = { /*TODO*/ }) {
+                    Button(onClick = { onSubmitButtonClick(ListDataTxt) }) {
+                        Text(text = stringResource(id = R.string.submit))
+                    }
+                }
+            }
+        }
     }
 }
